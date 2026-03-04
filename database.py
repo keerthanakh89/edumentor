5ba3857412193516b957c562dd9e22368ed6b9a5
KEERTHANA K H <keerthanakh01@gmail.com>
2026-03-04 22:16:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
 DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/edumentor"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
